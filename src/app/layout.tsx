1835effe3c1167a3f0fa7afac77e0cee8e796171
Kaushik Mondal <kaushikmondal.dev@gmail.com>
2026-05-21 13:55:52 +0530

    Add date-fns, bg images; update layout/page
    Add date-fns dependency and include light/dark background images in public/. Update layout to apply the new background images on <body>, remove the imported font variables and their classNames, and keep theme provider setup. Update the home page metadata to "Digital Clock App" and simplify the page markup to a minimal container for the clock UI. bun.lock was updated to reflect the new dependency.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import Header from "@/components/Header/Header";
 import ThemeProvider from "@/components/Providers/ThemeProvider";
-import { geistMono, geistSans } from "@/lib/fonts";
 import { ReactNode } from "react";
 import "./globals.css";
 
@@ -12,9 +11,8 @@ const RootLayout = ({ children }: Readonly<RootLayoutProps>) => {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       suppressHydrationWarning>
-      <body>
+      <body className="bg-[url(/white-bg.jpg)] bg-cover bg-no-repeat dark:bg-[url(/dark-bg.jpg)]">
         <ThemeProvider
           attribute={"class"}
           defaultTheme="dark"
